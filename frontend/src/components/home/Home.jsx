ee42fc72948388baae987134d08bccd79218bbf5
michelangelo7 <m_che_l@hotmail.com>
2021-02-11 20:28:30 -0300

    Created Routes

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Simple Products Stock">
+        <div className="display-4 ">Bem vindo</div>
+        <hr />
+        <p className="mb-0"> Sistena de controle de estoque simples, desenvolvido em React-js</p>
+    </Main>
---
+++ frontend/src/main/Routes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import Home from '../components/home/Home'
+
+export default props =>
+    <Switch>
+        <Route exact path='/' component={Home} />
+    </Switch>
