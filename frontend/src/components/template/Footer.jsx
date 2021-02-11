d94cc19a7be7b448598fb3d264123f19126ca5a3
michelangelo7 <m_che_l@hotmail.com>
2021-02-11 18:14:30 -0300

    Created Footer template

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Masg<span className="text-danger">Tec</span></strong>
+        </span>
+    </footer>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,12 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import './App.css'
 
 import Logo from '../components/template/Logo'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
 
 export default props =>
     <div className="app">
         <Logo />
         Nav
-        main
-        footer
+        <Main />
+        <Footer />
     </div>
