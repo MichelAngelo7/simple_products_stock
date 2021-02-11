7e191bb21671fb37397438b32e75d5f1c7a2ee14
michelangelo7 <m_che_l@hotmail.com>
2021-02-11 16:37:25 -0300

    Create Logo template

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+import Logo from '../components/template/Logo'
+
+
+export default props =>
+    <div className="app">
+        <Logo />
+        Nav
+        main
+        footer
+    </div>
